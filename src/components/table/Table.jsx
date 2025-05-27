74c7d4cfb5bf8c2c284ba867c00aef1c96bbd366
Josh Urdnb <joshua.urdanabia@gmail.com>
2025-05-27 15:43:29 +0200

    feat(table): ajout du composant Table avec tri, recherche, pagination et sélecteur de lignes
    - Installation de @tanstack/react-table (v8) pour la gestion avancée des tableaux
    - Création d’un composant <Table /> réutilisable avec :
      - Tri dynamique par colonne avec indicateurs visuels (↕, ↑, ↓)
      - Recherche globale avec filtrage en temps réel via globalFilter
      - Pagination avec navigation (précédent / suivant) et affichage du numéro de page actuel
      - Sélecteur du nombre de lignes à afficher par page (10, 25, 50, 100)
      - Affichage de la pagination dynamique du nombre de résultats visibles : "Showing X to Y of Z entries"
    - Gestion des états de tri, filtre et pagination entièrement contrôlée via TanStack React Table
    - Ajout d’un style personnalisé en CSS
    - Modification CSS dans home.css

---
+++ src/components/table/Table.jsx
@@ -0,0 +1,146 @@
+import './table.css'
+import React from 'react'
+
+// Importation des hooks et fonctions utilitaires de React Table
+import {
+    useReactTable,              // Hook principal pour créer une instance de table
+    getCoreRowModel,            // Gère les lignes de base (sans tri, filtre, etc.)
+    getSortedRowModel,          // Gère le tri des lignes
+    getPaginationRowModel,      // Gère la pagination
+    getFilteredRowModel,        // Import du modèle de filtrage (recherche)
+    flexRender,                 // Fonction pour rendre dynamiquement les cellules
+} from '@tanstack/react-table'
+
+const Table = ({ data, columns }) => {
+    const [sorting, setSorting] = React.useState([]) // État local pour suivre le tri appliqué
+    const [globalFilter, setGlobalFilter] = React.useState('') // État pour la recherche
+
+    // Création de l'instance de la table
+    const table = useReactTable({
+        data,                   // Données à afficher
+        columns,                // Colonnes (définies dans EmployeeList)
+        state: {
+            sorting,           // État de tri (contrôlé par React)
+            globalFilter,      // État de recherche (contrôleur par React) Appliquer le filtre global
+        },
+        onSortingChange: setSorting,              // Callback pour mettre à jour le tri
+        onGlobalFilterChange: setGlobalFilter,    // Callback de changement de filtre (recherche)
+        getCoreRowModel: getCoreRowModel(),       // Modèle de base
+        getSortedRowModel: getSortedRowModel(),   // Modèle après tri
+        getPaginationRowModel: getPaginationRowModel(), // Modèle avec pagination
+        getFilteredRowModel: getFilteredRowModel(),     // Utiliser le modèle de filtrage (recherche)
+    })
+
+    return (
+        <>
+            <div className='header-form'>
+                {/* Ajout du sélecteur de nombre de lignes à afficher par page */}
+                <div className="page-size-selector">
+                    <label className='show-entries'>Show entries:</label>
+                    <select
+                        value={table.getState().pagination.pageSize}
+                        onChange={e => table.setPageSize(Number(e.target.value))}
+                    >
+                        {[10, 25, 50, 100].map(pageSize => (
+                            <option key={pageSize} value={pageSize}>
+                                {pageSize}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                {/* Champ de recherche */}
+                <input className='search-input'
+                    type="text"
+                    placeholder="Search..."
+                    value={globalFilter}
+                    onChange={(e) => setGlobalFilter(e.target.value)}
+                />
+            </div>
+            <div className="table-wrapper">
+                {/* Table HTML */}
+                <table border="1" cellPadding="5" cellSpacing="0" style={{ borderCollapse: 'collapse', width: '100%' }}>
+                    <thead>
+                        {/* Boucle sur les groupes d'en-têtes (utile si colonnes imbriquées) */}
+                        {table.getHeaderGroups().map(headerGroup => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map(header => (
+                                    <th
+                                        key={header.id}
+                                        onClick={header.column.getToggleSortingHandler()} // Gestion du tri au clic
+                                        style={{ cursor: 'pointer' }}
+                                    >
+                                        {/* Rendu du nom de colonne (header) */}
+                                        {flexRender(header.column.columnDef.header, header.getContext())}
+
+                                        {/* Indicateur de tri (flèche montante/descendante) */}
+                                        <span className={header.column.getIsSorted() ? "table-sort-icon active" : "table-sort-icon"}>
+                                            {{
+                                                asc: '↑',
+                                                desc: '↓',
+                                            }[header.column.getIsSorted()] || '↕'}
+                                        </span>
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+
+                    <tbody>
+                        {/* Boucle sur les lignes à afficher */}
+                        {table.getRowModel().rows.map(row => (
+                            <tr key={row.id}>
+                                {/* Boucle sur les cellules visibles de chaque ligne */}
+                                {row.getVisibleCells().map(cell => (
+                                    <td key={cell.id}>
+                                        {/* Rendu de la cellule (texte ou composant) */}
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+            <div className='footer-form'>
+                {/* Affichage de la pagination : nombre d'entrées visibles */}
+                {/* Affichage des informations de pagination : indique le numéro de la première et dernière ligne affichée, ainsi que le total de lignes filtrées. */}
+                <div className='showing' style={{ marginTop: '8px' }}>
+                    <span>
+                        Showing {table.getState().pagination.pageIndex * table.getState().pagination.pageSize + 1}
+                        {' to '}
+                        {Math.min(
+                            (table.getState().pagination.pageIndex + 1) * table.getState().pagination.pageSize,
+                            data.length
+                        )}{' '}
+                        of {data.length} entries
+                    </span>
+                </div>
+
+                {/* Boutons de navigation pour la pagination */}
+                <div style={{ marginTop: '8px' }}>
+                    <button className='pagination-btn'
+                        onClick={() => table.previousPage()}                    // Aller à la page précédente
+                        disabled={!table.getCanPreviousPage()}                 // Désactiver si déjà en première page
+                    >
+                        Previous
+                    </button>
+
+                    <span className='pagination-index' style={{ margin: '0 10px' }}>
+                        {table.getState().pagination.pageIndex + 1}             {/* Numéro de page actuelle */}
+                    </span>
+
+                    <button className='pagination-btn'
+                        onClick={() => table.nextPage()}                        // Aller à la page suivante
+                        disabled={!table.getCanNextPage()}                     // Désactiver si en dernière page
+                    >
+                        Next
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Table
--- src/pages/employees/Employees.jsx
+++ src/pages/employees/Employees.jsx
@@ -1,11 +1,30 @@
 import './employees.css'
+import Table from '../../components/table/Table'
+import jsonData from '../../data/employees_data.json'
 
-export default function Home() {
+const EmployeeList = () => {
+    const data = jsonData.employeesData
+
+    const columns = [
+        { header: 'First Name', accessorKey: 'firstName' },
+        { header: 'Last Name', accessorKey: 'lastName' },
+        { header: 'Start Date', accessorKey: 'startDate' },
+        { header: 'Department', accessorKey: 'department' },
+        { header: 'Date of Birth', accessorKey: 'dateOfBirth' },
+        { header: 'Street', accessorKey: 'street' },
+        { header: 'City', accessorKey: 'city' },
+        { header: 'State', accessorKey: 'state' },
+        { header: 'Zip Code', accessorKey: 'zipCode' },
+    ]
+
     return (
-        <div className='employee-page'>
-            <div id="employee-div" className="container">
-                <h1>Current Employees</h1>
+        <div className="form-container">
+            <h1>Current Employees</h1>
+            <div className="table-employee-wrapper">
+                <Table data={data} columns={columns} />
             </div>
         </div>
     )
 }
+
+export default EmployeeList
